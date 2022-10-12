2c7cce4e0156284ea29f953656ea1dc2e696f508
ARMAN UMAR <97071435+ammangdeveloper@users.noreply.github.com>
2022-10-12 20:00:24 +0700

    DHT11

--- DHT11.ino
+++ DHT11.ino
@@ -1,28 +1,30 @@
-#include <DHT.h> //LIBRARY DHT
-int relayKipas = 5;
-DHT dht(D6, DHT11);  //Pin, Jenis DHT
+#include <DHT.h> //import library dht untuk dapat menggunakan sensor dht
+int relayKipas = 5; //pin input relay yg dihubungkan ke pin 5 arduino
+DHT dht(7, DHT11);  //pin out dht11 yg dihubungkan ke pin 7 arduino, Jenis DHT yg digunakan adalah DHT11
 
 void setup() {
-  pinMode(relayKipas, OUTPUT); //set relayKipas sebagai output
-  Serial.begin(9600);
-  dht.begin();
+  pinMode(relayKipas, OUTPUT); //setting relayKipas sebagai output
+  Serial.begin(9600); //komunikasi pada serial monitor
+  dht.begin(); //untuk memulai pembacaan sensor dht
 }
 
 void loop() {
-  float kelembaban = dht.readHumidity(); //membaca kelembapan
+
+  float kelembapan = dht.readHumidity(); //membaca kelembapan
   float suhu = dht.readTemperature(); //membaca suhu
 
-  Serial.print("kelembaban: ");
-  Serial.print(kelembaban);
+  Serial.print("kelembapan: ");
+  Serial.print(kelembapan);
   Serial.print(" ");
   Serial.print("suhu: ");
   Serial.println(suhu);
-  if(suhu > 30){
-    digitalWrite(relayKipas, HIGH);
-    Serial.println("Nyala");
-  }else{
-    digitalWrite(relayKipas, LOW);
-    Serial.println("Mati");
+  
+  if(suhu > 30){ //jika suhu lebih dari 30 derajat
+    digitalWrite(relayKipas, HIGH); //membuat kipas nyala
+    Serial.println("Kipas Nyala");
+  }else{ //30 derajat atau dibawah
+    digitalWrite(relayKipas, LOW); //membuat kipas mati
+    Serial.println("Kipas Mati");
   }
-  delay(1000);
+  
 }
